170aa8f514df91d9630dcbd16e9c7a1084b70942
Matteo Falkenberg <matteo.falkenberg@htl-wels.at>
2021-01-21 16:58:42 +0100

    bug fixes

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,6 +1,6 @@
 /**class Model
  * @author Matteo Falkenberg
- * @version 1.4, 21.01.2021
+ * @version 1.5, 21.01.2021
  */
 
 package model;
@@ -9,11 +9,11 @@ import java.util.Scanner;
 
 public class Model {
 
-    private ModularCounter red;
-    private ModularCounter green;
-    private ModularCounter blue;
+    private ModularCounter red = new ModularCounter(0, 256);
+    private ModularCounter green = new ModularCounter(0, 256);
+    private ModularCounter blue = new ModularCounter(0, 256);
 
-    Model(){
+    public Model(){
 
     }
 
@@ -24,6 +24,7 @@ public class Model {
         if(cc.equals(ColorCode.RED)){
             red.reset();
             red.inc(valInt);
+            System.out.println("value: " + valInt + "\ncolValue: " + getRed());
         }
 
         else if(cc.equals(ColorCode.GREEN)){
@@ -138,25 +139,28 @@ public class Model {
         int g = getGreen();
         int b = getBlue();
 
-        String hex = String.format("#%02x%02x%02x", r, g, b);
+        String hex = String.format("#%02X%02X%02X", r, g, b);
         return hex;
     }
 
 
     public String toString(){
-        String s = "State: " + this + "\n";
-        return s;
+        StringBuilder sb = new StringBuilder();
+        sb.append("Model[red=" + red.getValue() + "(mod " + red.getModulus() + "), ");
+        sb.append("green=" + green.getValue() + "(mod " + green.getModulus() + "), ");
+        sb.append("blue=" + blue.getValue() + "(mod " + blue.getModulus() + ")}");
+        return sb.toString();
     }
 
 
     public static void main(String[] args){
         Model model = new Model();
-        System.out.println(model.toString());
+        System.out.println("State: " + model.toString() + "\n");
 
         Scanner sc = new Scanner(System.in);
 
         while(true){
-            System.out.println("\nMenu");
+            System.out.println("Menu");
             System.out.println("  a - changeColorViaAbsoluteValue");
             System.out.println("  r - changeColorViaRelativeValue");
             System.out.println("  ? - view all accessors");
@@ -183,10 +187,10 @@ public class Model {
                     continue;
                 }
 
-                System.out.println("  Enter value (0-255");
+                System.out.println("  Enter value (0-255)");
                 String valSel = sc.next();
                 model.changeColorViaAbsoluteValue(colSel, valSel);
-                System.out.println(model.toString());
+                System.out.println("State: " + model.toString() + "\n");
             }
 
             else if(input.equals("r")){
@@ -208,17 +212,17 @@ public class Model {
                     continue;
                 }
 
-                System.out.println("  Enter value (0-255");
+                System.out.println("  Enter value (0-255)");
                 String valSel = sc.next();
                 model.changeColorViaRelativeValue(colSel, valSel);
-                System.out.println(model.toString());
+                System.out.println("State: " + model.toString() + "\n");
             }
 
             else if(input.equals("?")){
                 System.out.println("Red: " + model.getRed());
                 System.out.println("Green: " + model.getGreen());
                 System.out.println("Blue: " + model.getBlue());
-                System.out.println("Hex: " + model.getHex());
+                System.out.println("Hex: " + model.getHex() + "\n");
             }
 
             else if(input.equals("q")){
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,10 @@
 /**class Controller
  * @author Matteo Falkenberg
- * @version 1.4, 21.01.2021
+ * @version 1.5, 21.01.2021
  */
 
 package sample;
 
 public class Controller {
+
 }
